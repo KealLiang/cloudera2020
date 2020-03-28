e98b0743124f3e7f8f1d86680c66ea4ff7c37584
liangsr1 <lsrmod2014@163.com>
2020-03-28 23:27:12 +0800

    config files

--- cloud-config-center3344/src/main/java/com/kealliang/springcloud/ConfigCenter3344.java
+++ cloud-config-center3344/src/main/java/com/kealliang/springcloud/ConfigCenter3344.java
@@ -1,10 +1,19 @@
 package com.kealliang.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
 /**
  * @author lsr
  * @ClassName ConfigCenter3344
  * @Date 2020-03-28
  * @Vertion 1.0
  */
+@EnableConfigServer
+@SpringBootApplication
 public class ConfigCenter3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenter3344.class, args);
+    }
 }
